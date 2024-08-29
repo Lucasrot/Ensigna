ba70bb732112af866be3d4f8e4fb5aa05fa0bd88
Lucas Roteño <lucasrot1@hotmail.com>
2024-08-29 15:38:57 -0300

    Lucas

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import Card from 'react-bootstrap/Card';
-import ItemCount from "../ItemCount/ItemCount.jsx";
+import ItemCount from "../ItemCount/ItemCount";
 import { useCartContext } from '../../context/CartContext';
 
 const ItemDetail = ({id, name, description, price, image, stock}) => {   
